605114b6b800d00ef9a9e5c89fe0e8ff52aff7d8
RojasxAndres <afrojaslondono@gmail.com>
2021-05-06 09:00:40 -0500

    Añadiendo servicios

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,HttpResponse
-
+from servicios.models import Servicio
 # Create your views here.
 def home(req):
     return render(req,"ProyectoWebApp/home.html")
 
 def servicios(req):
-    return render(req,"ProyectoWebApp/servicios.html")
+    servicios = Servicio.objects.all() # Traiga todos los servicios que esten creados
+
+    return render(req,"ProyectoWebApp/servicios.html",{"servicios":servicios})
 
 def tienda(req):
     return render(req,"ProyectoWebApp/tienda.html")
