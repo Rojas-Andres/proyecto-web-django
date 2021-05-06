e1d19834c29db80601dc55159f69341d1cddfdb5
RojasxAndres <afrojaslondono@gmail.com>
2021-05-05 23:34:39 -0500

    Inicializando proyecto

---
+++ ProyectoWebApp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def home(req):
+    return render(req,"ProyectoWebApp/home.html")
+
+def servicios(req):
+    return render(req,"ProyectoWebApp/servicios.html")
+
+def tienda(req):
+    return render(req,"ProyectoWebApp/tienda.html")
+
+def blog(req):
+    return render(req,"ProyectoWebApp/blog.html")
+
+def contacto(req):
+    return render(req,"ProyectoWebApp/contacto.html")
---
+++ servicios/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Servicio(models.Model):
+    titulo    = models.CharField(max_length=50)
+    contenido = models.CharField(max_length=50)
+    imagen    = models.ImageField()
+    created   = models.DateTimeField(auto_now_add=True) # Agregar la fecha automaticamente
+    updated   = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = 'servicio' # El nombre que va a tener en la base de datos 
+        verbose_name_plural="servicios"
+    
+    def __str__(self):
+        return "El titulo es {} ".format(self.titulo)
