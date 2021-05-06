a9d5c1b0ac3892c2f8271cf55c5e94fd0daa00e1
RojasxAndres <afrojaslondono@gmail.com>
2021-05-06 14:40:15 -0500

    Modificando el blog.html

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from blog.models import Categoria,Post
 # Create your views here.
 
 def blog(req):
-    post = Post.objects.all()
-    return render(req,"blog/blog.html",{"post":post})
+    posts = Post.objects.all()
+    return render(req,"blog/blog.html",{"posts":posts})
