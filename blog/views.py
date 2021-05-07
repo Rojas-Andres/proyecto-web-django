76fcb208d99f36e79572d46f0f9d8649a3320d26
RojasxAndres <afrojaslondono@gmail.com>
2021-05-07 08:01:31 -0500

    Modificando categoria.html

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,13 @@ from blog.models import Categoria,Post
 def blog(req):
     posts = Post.objects.all()
     return render(req,"blog/blog.html",{"posts":posts})
+def categoria(req,categoria_id):
+    print(categoria_id,type(categoria_id))
+    categorias = Categoria.objects.get(id=categoria_id)
+    print(categorias)
+    posts = Post.objects.all()
+    print(posts)
+    posts = Post.objects.filter(categorias=categorias)
+    #print("\n\n categorias {} \n Post : {}",categorias,posts)
+    return render(req,"blog/categoria.html",{"categoria":categoria,"posts":posts})
+    #return render(req,"blog/blog.html")#,{"posts":posts})
