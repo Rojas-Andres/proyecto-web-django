fa74b3f929748786b31e2215786d5e6c1ddd5e08
RojasxAndres <afrojaslondono@gmail.com>
2021-05-07 11:27:22 -0500

    Creando formulario de contacto

---
+++ contacto/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+#Formulario de contacto
+class FormularioContacto(forms.Form):
+    nombre    = forms.CharField(label="Nombre",required=True)
+    email     = forms.CharField(label="Email",required=True)
+    contenido = forms.CharField(label="Contenido")
--- contacto/views.py
+++ contacto/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .forms import FormularioContacto
 # Create your views here.
 def contacto(req):
-    return render(req,"contacto/contacto.html")
+    formulario_contacto = FormularioContacto() # Creamos una instancia de la clase
+
+    return render(req,"contacto/contacto.html",{"miFormulario":formulario_contacto})
