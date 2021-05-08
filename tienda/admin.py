ef6633e82006c8a89fd79841eaa56b8f9c83f6f4
RojasxAndres <afrojaslondono@gmail.com>
2021-05-08 17:26:30 -0500

    Modificando tienda

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from tienda import models
+from tienda.models import Producto,CategoriaProd
 # Register your models here.
 
-#admin.site.register()
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+
+class CategoriaProddmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+
+
+admin.site.register(Producto,ProductoAdmin)
+admin.site.register(CategoriaProd,CategoriaProddmin)
